e798484fccd165b68cd6ad64b96b19b887760e3c
Gerard <gerardo_115@hotmail.es>
2024-05-17 08:23:03 -0500

    Update photon settings

--- Assets/Code/Network/GameConstData.cs
+++ Assets/Code/Network/GameConstData.cs
@@ -2,7 +2,7 @@
 public static class GameConstData
 {
     public static string ROOM_NAME = "scoreroom";
-    public static string URL = "https://html-classic.itch.zone/html/10443612/CokeGame/index.html";
+    public static string URL = "https://html-classic.itch.zone/html/10443944/CokeGame/index.html";
     public static float GAME_DURATION = 60f;
     public static string HEADER_ROW = "Nombre, Email, Telefono, Score";
 }
